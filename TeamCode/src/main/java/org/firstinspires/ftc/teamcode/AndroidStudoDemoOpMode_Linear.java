c0b294dc4434ecad973253a542d3d22817030b46
josef <Josef.Furman@gmail.com>
2018-10-14 12:39:52 -0500

    control + move and auto turn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AndroidStudoDemoOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AndroidStudoDemoOpMode_Linear.java
@@ -32,6 +32,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -49,7 +50,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="control mode", group="Linear Opmode")
+@TeleOp(name="single stick control mode", group="Linear Opmode")
 //@Disabled
 public class AndroidStudoDemoOpMode_Linear extends LinearOpMode {
 
@@ -66,13 +67,13 @@ public class AndroidStudoDemoOpMode_Linear extends LinearOpMode {
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        leftDrive  = hardwareMap.get(DcMotor.class, "motorL");
+        rightDrive = hardwareMap.get(DcMotor.class, "motorR");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -88,21 +89,36 @@ public class AndroidStudoDemoOpMode_Linear extends LinearOpMode {
             // Choose to drive using either Tank Mode, or POV Mode
             // Comment out the method that's not used.  The default below is POV.
 
-            // POV Mode uses left stick to go forward, and right stick to turn.
+            // POV Mode uses left stick to go forward, and right sticSk to turn.
             // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x;
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+            double drive =  gamepad1.left_stick_y;
+            double turn  =  gamepad1.left_stick_x;
 
+            if(turn < -0.5) {
+                leftPower = turn;
+                rightPower = -turn;
+                leftDrive.setPower(leftPower);
+                rightDrive.setPower(rightPower);
+            }
+            if(turn > 0.5){
+                rightPower = turn;
+                leftPower = -turn;
+                leftDrive.setPower(leftPower);
+                rightDrive.setPower(rightPower);
+            }
+            if(turn <= 0.5 && turn >= -0.5){
+                rightPower = drive;
+                leftPower = drive;
+                leftDrive.setPower(leftPower);
+                rightDrive.setPower(rightPower);
+            }
             // Tank Mode uses one stick to control each wheel.
             // - This requires no math, but it is hard to drive forward slowly and keep straight.
             // leftPower  = -gamepad1.left_stick_y ;
             // rightPower = -gamepad1.right_stick_y ;
 
             // Send calculated power to wheels
-            leftDrive.setPower(leftPower);
-            rightDrive.setPower(rightPower);
+
 
             // Show the elapsed game time and wheel power
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoProto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoProto.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
@@ -21,13 +22,13 @@ public class autoProto extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        leftDrive = hardwareMap.get(DcMotor.class, "motorL");
+        rightDrive = hardwareMap.get(DcMotor.class, "motorR");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
         /*
          * Initialize the drive system variables.
          * The init() method of the hardware class does all the work here
@@ -51,7 +52,7 @@ public class autoProto extends LinearOpMode {
         }
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-
+//hi
     }
 
     /*
